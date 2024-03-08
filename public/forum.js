be4403034740be9352b9f69b4370fdb03b5db647
Mahesh Langote <maheshlangote777@gmail.com>
2024-03-08 14:17:51 +0530

    Enhanced code

--- public/forum.js
+++ public/forum.js
@@ -1,4 +1,3 @@
-// Attach event listener to form submission
 import backendURI from "./config.js"; 
 const loggedEmail = sessionStorage.getItem('loggedEmail');
  
@@ -15,15 +14,14 @@ fetch(`${backendURI}/channels/createdBy/${adminEmail}`)
         channels.forEach(channel => {
             const channelLink = document.createElement('a');
             channelLink.textContent = channel.name;
-            channelLink.href = `chatDashboard.html?channelId=${channel._id}`; // Pass the channel ID in the URL
+            channelLink.href = `chatDashboard.html?channelId=${channel._id}`;  
             
-            // Create paragraph elements to display channel name and description
             const channelNameParagraph = document.createElement('p');
             channelNameParagraph.textContent = `Channel Name: `;
             const channelDescriptionParagraph = document.createElement('p');
             channelDescriptionParagraph.textContent = `Channel Description: ${channel.description}`;
         
-            // Append link and channel information to the channelsList element
+            
             channelsList.appendChild(channelLink);
             channelsList.appendChild(channelNameParagraph);
             channelsList.appendChild(channelDescriptionParagraph);
@@ -38,23 +36,19 @@ fetch(`${backendURI}/channels/createdBy/${adminEmail}`)
     });
 
 
-// Attach event listener to form submission
 document.getElementById('create-channel-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent default form submission behavior
 
-    // Get form data
     const formData = new FormData(this);
     const channelName = formData.get('channel-name');
     const channelDescription = formData.get('channel-description');
 
-    // Construct the request body with user's email
     const requestBody = {
         name: channelName,
         description: channelDescription,
         adminEmail: loggedEmail // Add the logged-in user's email to the request body
     };
 
-    // Example: Send form data to backend server using fetch API
     fetch(`${backendURI}/channels`, {
         method: 'POST',
         headers: {
@@ -64,12 +58,9 @@ document.getElementById('create-channel-form').addEventListener('submit', functi
     })
     .then(response => response.json())
     .then(data => {
-        // Handle successful response from server
         console.log('Channel created successfully:', data);
-        // Optionally, you can update the UI to display the newly created channel
     })
     .catch(error => {
-        // Handle error
         console.error('Error creating channel:', error);
     });
 });
